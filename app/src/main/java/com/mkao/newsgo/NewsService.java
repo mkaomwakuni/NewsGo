7c281635345a2a08a72fdd09a3e5901e31ba4c75
mkao_dev <mkaoibrahim@gmail.com>
2022-03-02 22:45:52 +0300

    Seventh commit

--- app/src/main/java/com/mkao/newsgo/NewsService.java
+++ app/src/main/java/com/mkao/newsgo/NewsService.java
@@ -1,6 +1,9 @@
 package com.mkao.newsgo;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsService {
     public static final String KEY = "f36d74acc142444f96809b7b9395716b";
@@ -21,6 +24,14 @@ public class NewsService {
     private Retrofit createRetrofit() {
         return new Retrofit.Builder()
                 .baseUrl("https://newsapi.org/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(createHttpClient())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
+    }
+
+    private OkHttpClient createHttpClient() {
+        return null;
     }
 
 
