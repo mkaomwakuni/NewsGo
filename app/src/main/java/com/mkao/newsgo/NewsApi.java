bc18689be65cbc823b888cd2b0f4ea219220425f
mkao_dev <mkaoibrahim@gmail.com>
2022-03-02 22:51:03 +0300

    Eighth commit

--- app/src/main/java/com/mkao/newsgo/NewsApi.java
+++ app/src/main/java/com/mkao/newsgo/NewsApi.java
@@ -1,4 +1,13 @@
 package com.mkao.newsgo;
 
+import kotlin.reflect.KCallable;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface NewsApi {
+    @GET("top-headlines")
+    Call<NewsList> getBasedOnLanguage(@Query("language") String lang,
+                                      @Query("country")String country,
+                                      @Query("apiKey") String key);
 }
