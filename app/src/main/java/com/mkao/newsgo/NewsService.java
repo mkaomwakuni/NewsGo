267541447a6179194a288be546a8e1852b7c6776
mkao_dev <mkaoibrahim@gmail.com>
2022-03-03 21:55:15 +0300

    nineth commit

--- app/src/main/java/com/mkao/newsgo/NewsService.java
+++ app/src/main/java/com/mkao/newsgo/NewsService.java
@@ -4,9 +4,12 @@ import androidx.annotation.NonNull;
 
 import java.io.IOException;
 
+import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -42,10 +45,23 @@ public class NewsService {
             @NonNull
             @Override
             public Response intercept(@NonNull Chain chain) throws IOException {
-                final
+                final Request original = chain.request();
+                final HttpUrl originalHttpUrl = original.url();
+                final HttpUrl url = originalHttpUrl.newBuilder()
+                        //.addQueryParameter(api_key,KEY)
+                .build();
+                final Request.Builder requestBuilder= original.newBuilder()
+                        .url(url);
+                final Request request = requestBuilder.build();
+                return chain.proceed(request);
 
             }
-        })
+        });
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.level(HttpLoggingInterceptor.Level.BODY);
+        httpClient.addInterceptor(logging);
+
+        return httpClient.build();
     }
 
 
