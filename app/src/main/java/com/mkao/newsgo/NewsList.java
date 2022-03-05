7372c0c2e1f5a53ed8b1519401c3dfe207779634
mkao_dev <mkaoibrahim@gmail.com>
2022-03-05 21:50:52 +0300

    Tenth commit

--- app/src/main/java/com/mkao/newsgo/NewsList.java
+++ app/src/main/java/com/mkao/newsgo/NewsList.java
@@ -1,4 +1,12 @@
 package com.mkao.newsgo;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**unified liof news for all fragments*/
 public class NewsList {
+    public static RecyclerView.Adapter extends RecyclerView.Adapter<NewsHolder>{
+    private final List<NewsDTO> mList = new ArrayList<>();
+    }
 }
